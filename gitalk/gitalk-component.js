3f8624e7e09770317a07da29c570ca23ca49e84d
FlyingPigQAQ <samudaodan@126.com>
2020-04-21 15:40:08 +0800

    Site updated: 2020-04-21 15:40:08

--- gitalk/gitalk-component.js
+++ gitalk/gitalk-component.js
@@ -3297,7 +3297,7 @@ var GitalkComponent = function (_Component) {
       }).then(function (res) {
         if (res.data && res.data.access_token) {
           _this.accessToken = res.data.access_token;
-          _this.getUserInfo().then(function () {
+          _this.getUserInfo().getInit().then(function () {
             return _this.setState({ isIniting: false });
           }).catch(function (err) {
             console.log('err:', err);
--- gitalk/gitalk.js
+++ gitalk/gitalk.js
@@ -6789,7 +6789,7 @@ var GitalkComponent = function (_Component) {
       }).then(function (res) {
         if (res.data && res.data.access_token) {
           _this.accessToken = res.data.access_token;
-          _this.getUserInfo().then(function () {
+          _this.getUserInfo().getInit().then(function () {
             return _this.setState({ isIniting: false });
           }).catch(function (err) {
             console.log('err:', err);
